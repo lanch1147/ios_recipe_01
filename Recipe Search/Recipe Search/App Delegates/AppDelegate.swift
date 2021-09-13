e44e5c735d595b56dc41464baa7986ebf7552a35
lanch1147 <chu.hoang.lan@sun-asterisk.com>
2021-09-13 14:50:17 +0700

    initial project and fix convention
    Init project clear app delegate and scene delegate
    
    delete white space

---
+++ Recipe Search/Recipe Search/App Delegates/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Recipe Search
+//
+//  Created by Lan Chu on 9/13/21.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
